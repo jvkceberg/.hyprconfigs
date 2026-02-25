cd33acac338373ac082b6574b6013023118d797a
jvkceberg <jtic0524@gmail.com>
2026-02-26 05:05:10 +0900

    Add markdown previewer plugin for nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -24,5 +24,14 @@ return {
   	},
   },
 
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+    ft = { "markdown" },
+    config = function()
+      require("render-markdown").setup({})
+    end,
+  },
+
   { import = "plugins.ron" }
 }
